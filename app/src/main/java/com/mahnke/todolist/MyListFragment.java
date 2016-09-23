60986b9f4799b7853a967e609aacb6f140cc4b71
Matthew Mahnke <mahnkem@msoe.edu>
2016-09-23 09:34:28 -0500

    Now added delete for real

--- app/src/main/java/com/mahnke/todolist/MyListFragment.java
+++ app/src/main/java/com/mahnke/todolist/MyListFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -29,50 +28,6 @@ public class MyListFragment extends ListFragment
     public MyListFragment() {
         // Required empty public constructor
         todoItems = new ArrayList<>();
-    }
-
-    /**
-     * Called when the fragment's activity has been created and this
-     * fragment's view hierarchy instantiated.  It can be used to do final
-     * initialization once these pieces are in place, such as retrieving
-     * views or restoring state.  It is also useful for fragments that use
-     * {@link #setRetainInstance(boolean)} to retain their instance,
-     * as this callback tells the fragment when it is fully associated with
-     * the new activity instance.  This is called after {@link #onCreateView}
-     * and before {@link #onViewStateRestored(Bundle)}.
-     *
-     * @param savedInstanceState If the fragment is being re-created from
-     *                           a previous saved state, this is the state.
-     */
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        // get refs to UI elements
-        final EditText myEditText = (EditText) getView().findViewById(R.id.myEditText);
-        final ListView myListView = getListView();
-
-        // connect adapter for displaying todo list items in ListView
-        final ArrayAdapter<String> aa = new ArrayAdapter<>(getActivity(),
-                                                           android.R.layout.simple_list_item_1,
-                                                           todoItems);
-        setListAdapter(aa);
-        getListView().setOnItemClickListener(this);
-
-        // let user add items to todo list
-        myEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.ACTION_DOWN &&
-                    keyCode == KeyEvent.KEYCODE_ENTER) {
-                    // add items to the end because older items are probably more important
-                    todoItems.add(myEditText.getText().toString());
-                    aa.notifyDataSetChanged();
-                    myEditText.setText("");
-                    return true;
-                }
-                return false;
-            }
-        });
     }
 
     @Override
@@ -96,11 +51,42 @@ public class MyListFragment extends ListFragment
      */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        StringBuilder description = new StringBuilder("parent = ").append(parent);
-        description.append("\nview = ").append(view);
-        description.append("\nposition = ").append(position);
-        description.append("\nid = ").append(id);
-
         Toast.makeText(getActivity(), this.todoItems.get(position), Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Attach to list view once the view hierarchy has been created.
+     *
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // get refs to UI elements
+        final EditText myEditText = (EditText) view.findViewById(R.id.myEditText);
+
+        // connect adapter for displaying todo list items in ListView
+        final ArrayAdapter<String> aa = new ArrayAdapter<>(getActivity(),
+                                                           android.R.layout.simple_list_item_1,
+                                                           todoItems);
+        setListAdapter(aa);
+        getListView().setOnItemClickListener(this);
+
+        // let user add items to todo list
+        myEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER) {
+                    // add items to the end because older items are probably more important
+                    todoItems.add(myEditText.getText().toString());
+                    aa.notifyDataSetChanged();
+                    myEditText.setText("");
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
