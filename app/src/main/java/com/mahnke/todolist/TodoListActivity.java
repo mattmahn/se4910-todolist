b292dd040563fc12507a675894322cb45bd1f219
Matthew Mahnke <mahnkem@msoe.edu>
2016-10-07 10:55:30 -0500

    Lab 3 part 1

--- app/src/main/java/com/mahnke/todolist/TodoListActivity.java
+++ app/src/main/java/com/mahnke/todolist/TodoListActivity.java
@@ -1,10 +1,14 @@
 package com.mahnke.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -16,5 +20,15 @@ public class TodoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+        final Button secondActivityButton = (Button) findViewById(R.id.secondActivityButton);
+        final Intent intent = new Intent(this, SecondActivity.class);
+
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
