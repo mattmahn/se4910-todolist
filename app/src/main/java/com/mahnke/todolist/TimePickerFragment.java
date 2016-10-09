35ec5067851cfd59e061f4c36bf674202859367c
Matthew Mahnke <mahnkem@msoe.edu>
2016-10-09 00:18:47 -0500

    Lab 3 part 2

---
+++ app/src/main/java/com/mahnke/todolist/TimePickerFragment.java
@@ -0,0 +1,31 @@
+package com.mahnke.todolist;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        final int hour = c.get(Calendar.HOUR_OF_DAY);
+        final int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(view.getContext(), hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/mahnke/todolist/TodoListActivity.java
+++ app/src/main/java/com/mahnke/todolist/TodoListActivity.java
@@ -1,34 +1,24 @@
 package com.mahnke.todolist;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ButtonBarLayout;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.animation.Interpolator;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class TodoListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+    }
 
-        final Button secondActivityButton = (Button) findViewById(R.id.secondActivityButton);
-        final Intent intent = new Intent(this, SecondActivity.class);
+    public void showTimePickerDialog(View view) {
+        DialogFragment dialogFragment = new TimePickerFragment();
+        dialogFragment.show(getSupportFragmentManager(), "timePicker");
+    }
 
-        secondActivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent);
-            }
-        });
+    public void showDatePickerDialog(View view) {
+        DialogFragment dialogFragment = new DatePickerFragment();
+        dialogFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
